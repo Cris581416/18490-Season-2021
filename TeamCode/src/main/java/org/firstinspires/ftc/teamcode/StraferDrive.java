11db2a9596aa3038db44c40a1851454c393c9244
Cristobal Velazquez <cvleazquez581416@gmail.com>
2020-11-21 09:47:41 -0800

    Add speed tuner -- not working yet

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraferDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StraferDrive.java
@@ -12,6 +12,8 @@ public class StraferDrive extends LinearOpMode {
     double bL;
     double bR;
 
+    double tuner = 0.5;
+
     DcMotor tLMotor;
     DcMotor tRMotor;
     DcMotor bLMotor;
@@ -28,19 +30,27 @@ public class StraferDrive extends LinearOpMode {
 
         waitForStart();
         while(opModeIsActive()){
-            double r = Math.hypot(gamepad1.left_stick_x, gamepad1.left_stick_y);
-            double robotAngle = Math.atan2(gamepad1.left_stick_y, gamepad1.left_stick_x) - Math.PI / 4;
+            double r = Math.hypot(-gamepad1.left_stick_x, gamepad1.left_stick_y);
+            double robotAngle = Math.atan2(gamepad1.left_stick_y, -gamepad1.left_stick_x) - Math.PI / 4;
             double rightX = -gamepad1.right_stick_x;
-            tL = r * Math.cos(robotAngle) + rightX;
-            tR = r * Math.sin(robotAngle) - rightX;
-            bL = r * Math.sin(robotAngle) + rightX;
-            bR = r * Math.cos(robotAngle) - rightX;
+
+            if(gamepad1.dpad_up && tuner < 1.0){
+                tuner += 0.05;
+            } else if(gamepad1.dpad_down && tuner > 0.05){
+                tuner -= 0.5;
+            }
+
+            tL = tuner * (r * Math.cos(robotAngle) + rightX);
+            tR = tuner * (r * Math.sin(robotAngle) - rightX);
+            bL = tuner * (r * Math.sin(robotAngle) + rightX);
+            bR = tuner * (r * Math.cos(robotAngle) - rightX);
 
             tLMotor.setPower(tL);
             tRMotor.setPower(tR);
             bLMotor.setPower(bL);
             bRMotor.setPower(bR);
 
+            telemetry.addData("tuner: ", tuner);
             telemetry.addData("tL: ", tL);
             telemetry.addData("tR: ", tR);
             telemetry.addData("bL: ", bL);
